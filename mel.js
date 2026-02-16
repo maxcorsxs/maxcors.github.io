01c9a6a30464360cf334137e58d01eba92107b6e
maxcorsxs <maxcorsxs@gmail.com>
2026-02-16 09:17:36 +0200

    Add files via upload

---
+++ mel.js
@@ -0,0 +1,5 @@
+(function () {
+    'use strict';
+    Lampa.Storage.set('lampac_unic_id', 'kolopov@ukr.net');
+    Lampa.Utils.putScriptAsync(['http://melnor.mooo.com/online.js', function() {}]);
+})();
