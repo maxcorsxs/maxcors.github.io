75bbd617ef2141842dcfd39d034b50f7ba9a67b4
maxcorsxs <maxcorsxs@gmail.com>
2026-02-16 10:00:04 +0200

    Delete mel.js

--- mel.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    'use strict';
-    Lampa.Storage.set('lampac_unic_id', 'kolopov@ukr.net');
-    Lampa.Utils.putScriptAsync(['http://melnor.mooo.com/online.js', function() {}]);
-})();
